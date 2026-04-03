f164b92e83b26c394b26e68534a7e8a3f1d943b5
matheus <matheusluan04@gmail.com>
2026-04-02 22:35:53 -0300

    primeira parte do exercio MemeToNervoso

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string fraseResposta;
+string fraseMemeToNervoso;
+
+Console.Write("Fala oque voce acha de mim cara? ");
+fraseResposta = Console.ReadLine();
+
+fraseMemeToNervoso = $"{fraseMemeToNervoso.Remove(10)}... NÃO PERA. \n to nervoso";
+
+Console.WriteLine(fraseMemeToNervoso);
