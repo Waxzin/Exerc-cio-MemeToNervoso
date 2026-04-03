6f2fb9051c5f59fb6b39547749276a3783aeff5c
Matheus Luan <matheusluan04@gmail.com>
2026-04-03 08:46:44 -0300

    Conclusão do exercicio

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
-﻿string fraseResposta;
+﻿string fraseResposta, memeToNervoso = "Seu filha de uma";
 
 Console.Write("Fala oque voce acha de mim?");
 fraseResposta = Console.ReadLine()!;
 
- string memeToNervoso = 
+ memeToNervoso = 
     $"{memeToNervoso.Remove(10)}... NÃO PERA. \n to Nervoso"!;
 
 Console.WriteLine(memeToNervoso);
